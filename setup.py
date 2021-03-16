8463e03e1bfcdbf4a62bd90ee72ba60d3af07491
suryadheeshjith <Surya.dheeshjith@gmail.com>
2021-03-16 16:40:08 +0530

    Updated setup

--- setup.py
+++ setup.py
@@ -11,15 +11,14 @@ setup(name="cfxgb",
       packages=["cfxgb", "cfxgb.lib","cfxgb.lib.cascade","cfxgb.lib.estimators","cfxgb.lib.utils"],
       package_dir={"": "src"},
       install_requires=[
-            "numpy",
-            "pandas",
-            "argparse",
-            "imblearn",
-            "scikit-learn",
-            "joblib",
-            "psutil",
-            "scipy",
-            "simplejson"],
+            "numpy==1.16.4",
+            "pandas==0.24.2",
+            "joblib==0.13.2",
+            "psutil==5.7.0",
+            "scikit-learn==0.21.2",
+            "imblearn==0.5.0",
+            "scipy==1.2.1"
+            ],
         extras_require={
             "testing": ["pytest-cov"]
       }
